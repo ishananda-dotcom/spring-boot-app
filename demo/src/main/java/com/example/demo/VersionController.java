4222f86d1fcf8df80d0958f13947813772f83220
ishananda-dotcom <isha.nanda@kickdrumtech.com>
2026-02-27 12:54:32 +0530

    add spring app

---
+++ demo/src/main/java/com/example/demo/VersionController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class VersionController {
+
+    @Value("${app.version:unknown}")
+    private String appVersion;
+
+    @GetMapping("/version")
+    public Map<String, String> version() {
+        return Map.of("version", appVersion);
+    }
+}
